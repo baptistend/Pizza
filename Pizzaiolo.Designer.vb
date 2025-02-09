fb0f9f4c0c3475ddac8483c6e6407a4a126e3062
thnicolas5 <Theo.NICOLAS@etu.uca.fr>
2025-02-09 11:22:16 +0100

    [DEV] Pizzaioo interface avec machine  état

--- Pizzaiolo.Designer.vb
+++ Pizzaiolo.Designer.vb
@@ -22,18 +22,60 @@ Partial Class Pizzaiolo
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        lstCommandes = New ListView()
+        Commande = New ColumnHeader()
+        Statut = New ColumnHeader()
+        btnTerminer = New Button()
         SuspendLayout()
+        ' 
+        ' lstCommandes
+        ' 
+        lstCommandes.Columns.AddRange(New ColumnHeader() {Commande, Statut})
+        lstCommandes.FullRowSelect = True
+        lstCommandes.Location = New Point(10, 10)
+        lstCommandes.Name = "lstCommandes"
+        lstCommandes.Size = New Size(460, 250)
+        lstCommandes.TabIndex = 0
+        lstCommandes.UseCompatibleStateImageBehavior = False
+        lstCommandes.View = View.Details
+        ' 
+        ' Commande
+        ' 
+        Commande.Text = "Commande"
+        Commande.Width = 350
+        ' 
+        ' Statut
+        ' 
+        Statut.Text = "Statut"
+        Statut.Width = 100
+        ' 
+        ' btnTerminer
+        ' 
+        btnTerminer.Enabled = False
+        btnTerminer.Location = New Point(10, 302)
+        btnTerminer.Name = "btnTerminer"
+        btnTerminer.Size = New Size(111, 46)
+        btnTerminer.TabIndex = 1
+        btnTerminer.Text = "Terminer"
+        btnTerminer.UseVisualStyleBackColor = True
         ' 
         ' Pizzaiolo
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(btnTerminer)
+        Controls.Add(lstCommandes)
         Location = New Point(820, 0)
         Name = "Pizzaiolo"
         StartPosition = FormStartPosition.Manual
         Text = "Pizzaiolo"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents lstCommandes As ListView
+    Private WithEvents Statut As ColumnHeader
+    Friend WithEvents btnTerminer As Button
+    Friend WithEvents Commande As ColumnHeader
 
 End Class
--- Pizzaiolo.vb
+++ Pizzaiolo.vb
@@ -1,8 +1,68 @@
 ﻿Imports Pizza.Commande
 
 Public Class Pizzaiolo
+    Inherits Form
+
+    Private Enum EtatCommande
+        AucuneCommandeSelectionnee
+        CommandeSelectionnee
+    End Enum
+
+    Private commandesEnCours As New List(Of Commande)
+    Private EtatActuel As EtatCommande
+
+    ''' <summary>
+    ''' Ajoute une commande à la liste
+    ''' </summary>
     Public Async Sub CommandeRecue(commande As Commande)
-        Await Task.Delay(1000)
-        MessageBox.Show($"Commande reçue : {commande.afficherCommande()}")
+        Await Task.Delay(1000) ' Simulation de traitement
+        commandesEnCours.Add(commande)
+
+        ' Création de l'item et ajout à la ListView
+        Dim item As New ListViewItem(commande.AfficherCommande())
+        item.SubItems.Add("En cours")
+        lstCommandes.Items.Add(item)
+
+        MessageBox.Show($"Nouvelle commande reçue : {commande.AfficherCommande()}")
+
+        ' Mise à jour de l'état après ajout
+        ChangerEtat()
+    End Sub
+
+    ''' <summary>
+    ''' Gestion du bouton Terminer
+    ''' </summary>
+    Private Sub btnTerminer_Click(sender As Object, e As EventArgs) Handles btnTerminer.Click
+        If lstCommandes.SelectedItems.Count > 0 Then
+            Dim item As ListViewItem = lstCommandes.SelectedItems(0)
+            lstCommandes.Items.Remove(item)
+
+            MessageBox.Show("Commande terminée avec succès !")
+
+            ' Mise à jour de l'état après suppression
+            ChangerEtat()
+        Else
+            MessageBox.Show("Veuillez sélectionner une commande.")
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Met à jour l'état du bouton Terminer
+    ''' </summary>
+    Private Sub ChangerEtat()
+        If lstCommandes.SelectedItems.Count > 0 Then
+            EtatActuel = EtatCommande.CommandeSelectionnee
+            btnTerminer.Enabled = True
+        Else
+            EtatActuel = EtatCommande.AucuneCommandeSelectionnee
+            btnTerminer.Enabled = False
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Événement déclenché lorsqu'une commande est sélectionnée
+    ''' </summary>
+    Private Sub lstCommandes_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstCommandes.SelectedIndexChanged
+        ChangerEtat()
     End Sub
 End Class
